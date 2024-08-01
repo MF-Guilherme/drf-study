6addbc686f0803e0064649b326cf401be730aa99
Gui Montenegro <guimontenegro23@yahoo.com.br>
2024-08-01 20:42:20 -0300

    feat: endpoint para cadastrar curso e avaliação

--- escola/cursos/views.py
+++ escola/cursos/views.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
 # importar as models
 from .models import Curso, Avaliacao
@@ -12,8 +13,14 @@ class CursoAPIView(APIView):
     """ API de Cursos tal tal tal """
     def get(self, request):
         cursos = Curso.objects.all()
-        serializer = CursoSerializer(cursos, many=True) # many=True pq estou pegando "all objects" # noqa
+        serializer = CursoSerializer(cursos, many=True) # noqa - many=True pq estou pegando "all objects"
         return Response(serializer.data)
+
+    def post(self, request):
+        serializer = CursoSerializer(data=request.data) # noqa - receber os dados e serializar
+        serializer.is_valid(raise_exception=True) # noqa - verificar se estão válidos
+        serializer.save()  # salvar
+        return Response(serializer.data, status=status.HTTP_201_CREATED) # noqa - prepara uma resposta com os dados salvos e uma resposta HTTP 
 
 
 class AvaliacaoAPIView(APIView):
@@ -22,3 +29,9 @@ class AvaliacaoAPIView(APIView):
         avaliacoes = Avaliacao.objects.all()
         serializer = AvaliacaoSerializer(avaliacoes, many=True)
         return Response(serializer.data)
+
+    def post(self, request):
+        serializer = AvaliacaoSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
